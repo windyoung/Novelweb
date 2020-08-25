0353b3ec1b8547fdddd11bacc3e3bbf90873b4ee
windyoung <windyoung89@gmail.com>
2020-08-25 21:48:24 +0800

    2020-08-25 21:48 :

--- books_vue/books/src/App.vue
+++ books_vue/books/src/App.vue
@@ -1,9 +1,3 @@
-<!--
- * @Date: 2020-08-25 21:27:42
- * @LastEditors: zhujian
- * @LastEditTime: 2020-08-25 21:27:43
- * @FilePath: /Novelweb/books_vue/books/src/App.vue
--->
 <template>
   <div id="app">
 
